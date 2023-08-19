71665f81e7a732b0259f3c29b9b1f54563b1abb8
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-08-19 14:30:56 +0300

    mule

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,8 +3,19 @@ from models import app, db, User
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
-
+from flask_login import login_user, login_required, logout_user, current_user, LoginManager
 app = app
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Load the user from the database based on the user ID
+    user = None
+    if not user:
+        user = User.query.get(int(user_id))
+    return user
 
 @app.route('/')
 def index():
@@ -29,7 +40,10 @@ def signup():
         full_name = request.form['username']
         email = request.form['email']
         password = request.form['password']
-
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email address already exists', 'error')
+            return redirect(url_for('login'))
         # Hash the password
         hashed_password = generate_password_hash(password, method='sha256')
 
@@ -42,7 +56,7 @@ def signup():
         db.session.commit()
 
         flash('Your account has been created successfully. Please log in.', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('signup'))
 
     return render_template('signup.html')
 @app.route('/login', methods=['GET', 'POST'])
@@ -55,25 +69,28 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
             # Log in the patient
-            return render_template('homel.html')
+            login_user(user, remember=True)
+            return render_template('homel.html',user=current_user)
         return "no"
 
     return render_template('signup.html')
 
 @app.route('/homel')
+@login_required
 def homel():
-    return render_template('homel.html')
-
+    return render_template('homel.html', user=current_user)
 @app.route('/clubsl')
+@login_required
 def clubsl():
     return render_template('clubs-1.html')
 
 @app.route('/aboutl')
+@login_required
 def aboutl():
     return render_template('about-1.html')
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html',user=current_user)
 
 if __name__ == '__main__':
     with app.app_context():
